74b617f67e2f1c04c5e28a5379eed514a48feeb9
André <andrecooke@hotmail.com>
2015-11-12 21:45:59 -0500

    finalement ca marche... ajouter etudiant

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -12,8 +12,6 @@ namespace ConsoleApplication1
         {
             //menu();
 
-
-
             List<etudiant> etudiantListe = new List<etudiant>();
 
             etudiant et = new etudiant();
@@ -24,26 +22,74 @@ namespace ConsoleApplication1
             et = new etudiant("ABC02", "Asselin", "Maxime");
             etudiantListe.Add(et);
 
+            int valeurChoix = 0;
 
+            while (valeurChoix != 8)
+            {
+                valeurChoix = menu();
+
+                if (valeurChoix == 5)
+                    return;
+
+                //Prendre action / loadé le module correspondant
+                if (valeurChoix == 1)  // Option 1 - Ajouter Etudiant
+                {
+                    etudiantListe.Add(ajouterEtudiant());
+                }
+                else if (valeurChoix == 2) // Option 2 - Afficher un Étudiant 
+                {
+                    Console.WriteLine("Afficher etudiant");
+                }
+                else if (valeurChoix == 3) // Option 3 Modifier un Étudiant 
+                {
+                    Console.WriteLine("Afficher Modifier un Étudiant");
+                }
+                else if (valeurChoix == 4) // Option 4 - Afficher tous les Étudiants 
+                {
+                    Console.WriteLine("Afficher tous les Étudiants");
+                }
+                else if (valeurChoix == 5) // Option 5 - Sortir du programme 
+                {
+                    Console.WriteLine("Quitter");
+                }
+
+            }
             //Console.WriteLine(etudiantListe[0].ToString());
             //Console.WriteLine(etudiantListe[1].ToString());
             //Console.WriteLine(etudiantListe[2].ToString());
-            //etudiantListe.Add(ajouterEtudiant());
-            ajouterEtudiant();
 
         }
 
-        private static void menu() // affichage du menu
+        private static int menu() // affichage du menu
         {
             Console.Clear();
             Console.WriteLine("Exercise Etudiant");
             Console.WriteLine("1- entrer un nouvel étudiant");
             Console.WriteLine("2- afficher un étudiant");
-            Console.WriteLine("3- afficher tous les étudiants");
-            Console.WriteLine("4- Quitter");
+            Console.WriteLine("3- modifier un étudiant");
+            Console.WriteLine("4- afficher tous les étudiants");
+            Console.WriteLine("5- Quitter");
+
+            int valeurChoix;
+
+            try
+            {
+                // recup le nombre et faire un parse du resultat
+                valeurChoix = int.Parse(Console.ReadLine());
+                return valeurChoix;
+            }
+            catch
+            {
+                //erreur dans le nombre
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Erreur SVP Sélectionnez un nombre");
+                Console.ReadKey();
+                Console.ResetColor();
+                return 9;
+            }
         }
 
-        private static object ajouterEtudiant() // ajoute un etudiant
+        private static etudiant ajouterEtudiant() // ajoute un etudiant
         {
             etudiant et = new etudiant();
             string idEtudiant;
@@ -51,7 +97,7 @@ namespace ConsoleApplication1
             string prenomEtudiant;
             string noteEtudiant;
             int valNote;
-            
+
             Console.Clear();
             Console.WriteLine("Exercise Etudiant");
             Console.WriteLine("ID de l'etudiant :");
@@ -67,21 +113,25 @@ namespace ConsoleApplication1
             noteEtudiant = Console.ReadLine().ToUpper();
 
             bool noteValid = Int32.TryParse(noteEtudiant, out valNote);
-            if (noteValid == false)
+            if (!noteValid)
             {
                 Console.WriteLine("conversion de '{0}' a échouer.", noteEtudiant);
             }
-            
 
             et = new etudiant(idEtudiant, nomEtudiant, prenomEtudiant, valNote);
 
-            Console.WriteLine(et.ToString());
-
             return et;
-            
-
         }
 
+        private static void afficherEtudiant() // affiche un etudiant
+        {
+            string idEtudiant;
+            Console.WriteLine("indiquer le ID de l'etudiant :");
+            idEtudiant = Console.ReadLine().ToUpper();
+            
+            Console.WriteLine("Étudiant trouvé : {0}","code");
+
+        }
 
         class etudiant
         {
@@ -123,6 +173,11 @@ namespace ConsoleApplication1
             {
                 get { return prenomEtudiant; }
                 set { prenomEtudiant = value; }
+            }
+
+            public string note
+            {
+                get { return noteEtudiant.ToString(); }
             }
 
             public override string ToString()
